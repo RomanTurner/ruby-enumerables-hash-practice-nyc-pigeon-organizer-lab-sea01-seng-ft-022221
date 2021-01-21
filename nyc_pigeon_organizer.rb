541801a2e3cac4f270e31808916d91eea15586b4
Roman Turner <rn.turner@live.com>
2021-01-21 02:09:03 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,13 +3,16 @@ require 'pry'
 
 def nyc_pigeon_organizer(data)
   
-  data.each_with_object({}) do |(key, value), output|
+ final_results = data.each_with_object({}) do |(key, value), output|
  #  binding.pry
    value.each do |inner_key, names|
      names.each do |name|
        if !output[name] 
        output[name] = {}
         binding.pry
+       end
+       if !output[name][key]
+         output[name][key] = []
        end
        
    end
